ac8028a55ae896612293b2221a81149e8676d5fd
dannycruzga <dannyc@email.sc.edu>
2026-09-10 12:48:54 -0400

    Javascript boucning ball

--- csce242/exercises/04javascript/scripts/intro.js
+++ csce242/exercises/04javascript/scripts/intro.js
@@ -1,4 +1,16 @@
+/* Shows a mesage when button is clicked */
 document.getElementById("btn-show-message").onclick = (e) => {
     document.getElementById("p-message").innerHTML = "Hello World!";
     e.target.innerHTML = "Done";
-}
+};
+
+//Styles the link when it's clicked//
+document.getElementById("link").onclick = (e) => {
+    e.preventDefault(); //don't go to links destination
+    e.target.classList.add("cool-link");
+};
+
+//when button clicked make ball bounce//
+document.getElementById("btn-bounce").oneclick = (e) => {
+    document.getElementById("ball").classList.add("boucning-ball");
+};
